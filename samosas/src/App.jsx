5a98229d840d11a8434daecb13d734ed85f4ff08
alzi03 <alammzishan03@gmail.com>
2023-03-02 00:11:25 -0500

    samosas

---
+++ samosas/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+import Samosa from './components/Samosa.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Samosa />
+    </div>
+  )
+}
+
+export default App
