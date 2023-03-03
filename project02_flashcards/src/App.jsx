ada1962a4afe35b489ebca46ae5c3af49058cc89
alzi03 <alammzishan03@gmail.com>
2023-03-03 15:11:18 -0500

    project2 finalcommit

---
+++ project02_flashcards/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import Display from './components/Display.jsx'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Display />
+    </div>
+  )
+}
+
+export default App
---
+++ project02_flashcards/src/components/Flashcard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Flashcard = (props) => {
+  return(
+    <div className="flashcard" onClick={handleClick}>
+      
+    </div>
+  )
+}
+
+export default Flashcard;
