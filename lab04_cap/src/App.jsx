bfbd7ef37effaf6db2fcdfa112e3e29a27595719
alzi03 <alammzishan03@gmail.com>
2023-03-17 23:38:24 -0400

    name change

---
+++ lab04_cap/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import Display from './components/Display'
+
+const App = () =>  {
+  return (
+    <div>
+      <Display />
+    </div>
+  )
+}
+
+export default App;
